a9b76a9c7ffab0dd2dae36204614c1d728f92aca
SatriaHilmi <vanpers34@gmail.com>
2026-01-28 10:07:36 +0700

    feat: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  site: 'https://example.com',
   vite: {
     plugins: [tailwindcss()]
   },
