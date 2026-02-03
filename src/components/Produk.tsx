61c0a7bddc5acb7556d6d2e3a32ed2e41c8aba21
SatriaHilmi <vanpers34@gmail.com>
2026-02-03 07:39:19 +0700

    feat: add image

--- src/components/Produk.tsx
+++ src/components/Produk.tsx
@@ -2,6 +2,10 @@ import bg1 from "../assets/kayu_1.jpeg";
 import bg2 from "../assets/kayu_2.jpeg";
 import bg3 from "../assets/kayu_3.jpeg";
 import bg4 from "../assets/kayu_4.jpeg";
+import bg5 from "../assets/truk_1.jpeg";
+import bg6 from "../assets/truk_2.jpeg";
+import bg7 from "../assets/truk_3.jpeg"
+
 
 interface produkProops {
     // text: string;
@@ -15,20 +19,26 @@ export const Produk = () => {
         { img: bg2.src },
         { img: bg3.src },
         { img: bg4.src },
+        { img: bg5.src },
+        { img: bg6.src },
+        { img: bg7.src },
     ]
 
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mx-auto items-center my-5 px-10">
-                {produkData.map((item, index) => (
-                    <div className="max-w-4xl rounded-lg overflow-hidden shadow-lg border hover:border-green-500 transition hover:-translate-y-1 hover:scale-105 duration-300" key={index}>
-                        <img src={item.img} alt="gambar produk" className="w-full aspect-square object-cover" />
-                        {/* <div className="p-3 text-center">
-                            <p className="font-semibold text-sm md:text-md">{item.text}</p>
-                        </div> */}
-                    </div>
-                ))}
-            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mx-auto items-center my-5 px-10">
+                {produkData.map((item, index) => {
+                    const isLast = index === produkData.length - 1
+                    const isOdd = produkData.length % 3 === 1
+
+                    return (
+                        <div key={index} className={`max-w-4xl rounded-lg overflow-hidden shadow-lg border hover:border-green-500 transition hover:-translate-y-1 hover:scale-105 duration-300 ${isLast && isOdd ? "md:col-span-3 flex justify-center mx-auto" : ""}`
+                        }>
+                            <img src={item.img} alt="gambar produk" className={`w-full aspect-square object-cover ${isLast && isOdd ? "md:max-w-90" : ""}`} />
+                        </div>
+                    )
+                })}
+            </div >
         </>
     )
 }
